3bfa8a3b3206346fca16ca986ce4fa0f59061418
yun8boo <yuma051521@gmail.com>
2022-01-04 00:20:56 +0900

    commit

--- apps/frontend-e2e/src/integration/app.spec.ts
+++ apps/frontend-e2e/src/integration/app.spec.ts
@@ -1,13 +1,11 @@
-import { getGreeting } from '../support/app.po';
+import { getAddTodoButton, getTodo } from '../support/app.po';
 
-describe('frontend', () => {
+describe('TodoApps', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome frontend');
+  it('should display todos', () => {
+    getTodo().should((t) => expect(t.length).equal(2));
+    getAddTodoButton().click();
+    getTodo().should((t) => expect(t.length).equal(3));
   });
 });
